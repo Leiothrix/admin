1a4fde851e2de2765c67c13fd7dfbf7f9168dd7b
Leiothrix <Leiothrix@users.noreply.github.com>
2019-05-14 15:09:18 +0800

    common update

--- admin-common/src/main/java/com/paprika/utils/StringUtils.java
+++ admin-common/src/main/java/com/paprika/utils/StringUtils.java
@@ -1,9 +1,9 @@
-package com.paprika.aop.limit;
+package com.paprika.utils;
 
 /**
  * @author adam
- * @date 2019/5/13
+ * @date 2019/5/14
  * PS: You may say that I'm a dreamer.But I'm not the only one.
  */
-public @interface Limit {
+public class StringUtils {
 }
---
+++ admin-system/src/main/java/com/paprika/modules/quartz/service/QuartzJobService.java
@@ -0,0 +1,9 @@
+package com.paprika.modules.quartz.service;
+
+/**
+ * @author adam
+ * @date 2019/5/14
+ * PS: You may say that I'm a dreamer.But I'm not the only one.
+ */
+public interface QuartzJobService {
+}
